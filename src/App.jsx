505ab8a5f533355c64ee0337171df2fcfe9af0b9
kapaintsil <kwame.bixcuit@gmail.com>
2025-06-29 23:05:00 +0000

    fix: chabge the route of the homepage in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import Home from './Pages/Home/Home';     
-import SignUp from './Pages/SignUp/SignUp'; 
+import Home from './Pages/Home/Home.jsx';     
+import SignUp from './Pages/SignUp/SignUp.jsx'; 
 
 function App() {
   return (

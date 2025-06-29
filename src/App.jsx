1b267676188891c16a93fdc094fd5eaf5596ba8f
kapaintsil <kwame.bixcuit@gmail.com>
2025-06-29 19:29:31 +0000

    fix: change the 'pages to 'Pgaes' Changed 'page' to 'page' in the file path to make it easy to route

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import Home from './pages/Home/Home';     
-import SignUp from './pages/SignUp/SignUp'; 
+import Home from './Pages/Home/Home';     
+import SignUp from './Pages/SignUp/SignUp'; 
 
 function App() {
   return (
